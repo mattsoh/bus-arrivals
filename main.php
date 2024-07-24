2a6b3b910029141d4d21968be84f47ea300c72fd
Matthew <mattsoh100@gmail.com>
2024-07-24 09:40:25 +0800

    More edits

--- main.php
+++ main.php
@@ -6,4 +6,9 @@
     <link rel="stylesheet" href="styles.css"> 
 </head>
 <body>
-    
+    <h1>Find your bus arrival time!</h1>
+    <form action="request.php" method="post">
+        <label for="busStopNumber">Bus Stop Number:</label>
+        <input type="number" id="busStopNumber" name="busStopNumber" maxlength="5" pattern="[0-9]*" placeholder="bus no. goes here!" required>
+        <input type="submit" value="Submit">
+    </form>
