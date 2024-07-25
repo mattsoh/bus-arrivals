5c271fd7508264654cf13e50a784dffd0a29ca09
Matthew <mattsoh100@gmail.com>
2024-07-25 09:19:30 +0800

    something wrong with the API key get

--- timings.php
+++ timings.php
@@ -16,13 +16,23 @@
     <link rel="stylesheet" href="styles.css"> 
 </head>
 <body>
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $busStopCode = trim($_POST['busStopCode']);
+    <h1><?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $busStopCode = trim($_POST['busStopCode']);
 
-    if (!empty($busStopCode) && is_numeric($busStopCode) && strlen($busStopCode) == 5) {
-        echo "Stop " . htmlspecialchars($busStopCode) . "</h1>";
-
+        if (!empty($busStopCode) && is_numeric($busStopCode) && strlen($busStopCode) == 5) {
+            echo "Stop " . htmlspecialchars($busStopCode);
+        } else {
+            echo "Invalid bus stop code. Please enter a valid code.";
+            exit();
+        }
+    } else {
+        echo "Invalid request method.";
+        exit();
+    }
+    ?></h1>
+    <h3><?php echo "key".getenv("API_KEY");?></h3>
+    <?php
         $curl = curl_init();
         curl_setopt_array($curl, array(
             CURLOPT_URL => 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode='.$busStopCode,
@@ -33,11 +43,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-              'AccountKey: vDwU2kHERveeJzNNtB/7bw=='
-            ),
-          ));
-
+            CURLOPT_HTTPHEADER => array('AccountKey: ' . getenv("API_KEY")),
+        ));
         $response = curl_exec($curl);
         curl_close($curl);
 
@@ -60,7 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <th>Next Bus 3 Load</th>
                     <th>Next Bus 3 Feature</th>
                     <th>Next Bus 3 Type</th>
-                  </tr>";
+                </tr>";
             foreach ($data['Services'] as $service) {
                 echo "<tr>";
                 echo "<td>" . htmlspecialchars($service['ServiceNo']) . "</td>";
@@ -77,12 +84,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             echo "<h2>No bus services found at this bus stop. Check the bus stop code and try again.</h2>";
         }
-    } else {
-        echo "<h1>Invalid bus stop code. Please enter a valid code.</h1>";
-    }
-} else {
-    echo "<h1>Invalid request method.</h1>";
-}
-?>
+    ?>
 </body>
 </html>
