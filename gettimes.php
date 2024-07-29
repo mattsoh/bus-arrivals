1c8ecf43ae078ab7a88ed3f467d23d6bf1ddfe98
Matthew <mattsoh100@gmail.com>
2024-07-30 02:04:57 +0800

    added binary search

--- gettimes.php
+++ gettimes.php
@@ -76,6 +76,24 @@ function getStop($stop){
                 return $poss["Description"];
             }
         }
+        // function binarySearch($array, $target) {
+        //     $left = 0;
+        //     $right = count($array) - 1;
+        
+        //     while ($left <= $right) {
+        //         $mid = floor(($left + $right) / 2);
+        
+        //         if ($array[$mid] == $target) {
+        //             return $mid;
+        //         } elseif ($array[$mid] < $target) {
+        //             $left = $mid + 1;
+        //         } else {
+        //             $right = $mid - 1;
+        //         }
+        //     }
+        
+        //     return -1;
+        // }
     }
     
     // return $data;
