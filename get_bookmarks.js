8c761f2371565a46b45f01f29383aabb882467bc
Matthew <mattsoh100@gmail.com>
2024-08-01 08:20:28 +0800

    implementing links

--- get_bookmarks.js
+++ get_bookmarks.js
@@ -10,14 +10,26 @@ function getStops() {
         return[[],[]];
     }
 }
+
+function queryBookmark(stop) {
+    var form = document.busStopForm;
+    form.stop.value = stop;
+    form.submit();
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const [stops, stopNames] = getStops();
     const container = document.getElementById('bookmarked-stops');
     container.innerHTML = '';
-    console.log(stops)
-    for (var i = 0;i<stops.length;i++){
+    console.log(stops);
+    for (var i = 0; i < stops.length; i++) {
         const stopElement = document.createElement('div');
         stopElement.textContent = `${stopNames[i]} (${stops[i]})`;
+        stopElement.onclick = function() {
+            queryBookmark(stops[i]);
+            console.log(stops[i]);
+        };
+        console.log(stopElement.onclick);
         container.appendChild(stopElement);
     }
 });
