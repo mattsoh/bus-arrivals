d1550a132c64416cfa9e961cdbd445fc3f228289
Matthew <mattsoh100@gmail.com>
2024-08-01 09:29:06 +0800

    debugging

--- get_bookmarks.js
+++ get_bookmarks.js
@@ -1,8 +1,8 @@
 function getStops() {
     const stops = JSON.parse(localStorage.getItem('stops'));
     const stopNames = JSON.parse(localStorage.getItem('stopNames'));
-    console.log(stops,stopNames,(stops&&stopNames),stops.length == stopNames.length)
-    console.log(stops.length,stopNames.length)
+    // console.log(stops,stopNames,(stops&&stopNames),stops.length == stopNames.length)
+    // console.log(stops.length,stopNames.length)
     if (stops && stopNames && stops.length == stopNames.length){
         console.log("returning ", stops,stopNames)
         return [stops,stopNames];
@@ -21,15 +21,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const [stops, stopNames] = getStops();
     const container = document.getElementById('bookmarked-stops');
     container.innerHTML = '';
-    console.log(stops);
+    // console.log(stops);
     for (var i = 0; i < stops.length; i++) {
         const stopElement = document.createElement('div');
         stopElement.textContent = `${stopNames[i]} (${stops[i]})`;
-        stopElement.onclick = function() {
-            queryBookmark(stops[i]);
-            console.log(stops[i]);
-        };
-        console.log(stopElement.onclick);
+        stopElement.onclick = (function(stop) {
+            stopElement.onclick = function() {
+                queryBookmark(stop);
+            };
+        })(stops[i]);
+        // console.log(stopElement.onclick);
         container.appendChild(stopElement);
     }
 });
