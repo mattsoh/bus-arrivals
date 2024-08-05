74230e0805ab9c7b02097a4b469749590513778a
mattsoh <mattsoh100@gmail.com>
2024-08-06 01:07:27 +0800

    improvements

---
+++ getNearestStops.php
@@ -0,0 +1,60 @@
+<?php
+function getStops($stop){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['latitude']) && isset($_POST['longitude'])) {
+        $latitude = $_POST['latitude'];
+        $longitude = $_POST['longitude'];
+        $_SESSION['latitude'] = $latitude;
+        $_SESSION['longitude'] = $longitude;
+    }
+}
+function timings($busStopCode) {
+
+    $services = []; 
+    if (!empty($busStopCode) && is_numeric($busStopCode) && strlen($busStopCode) == 5) {
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=' . $busStopCode,
+            CURLOPT_RETURNTRANSFER => true,
+            // CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            // CURLOPT_FOLLOWLOCATION => true,
+            // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array('AccountKey: ' . getenv("API_KEY")),
+        ));
+        $response_data = curl_exec($curl);
+        curl_close($curl);
+        $data = json_decode($response_data, true);
+
+        if (!empty($data['Services'])) {
+            foreach ($data['Services'] as &$service) {
+                foreach (['NextBus', 'NextBus2'] as $nextBus) {
+                    if (!empty($service[$nextBus]['EstimatedArrival'])) {
+                        $estimatedArrival = new DateTime($service[$nextBus]['EstimatedArrival']);
+                        $now = new DateTime();
+                        
+                        $interval = $now->diff($estimatedArrival);
+                        $service[$nextBus]['TimeRemaining'] = ($interval->invert) ? [0,0] : [$interval->i,$interval->s];
+                        unset($service[$nextBus]['OriginCode']);
+                        unset($service[$nextBus]['DestinationCode']);
+                        unset($service[$nextBus]['EstimatedArrival']);
+                        unset($service[$nextBus]['Latitude']);
+                        unset($service[$nextBus]['Longitude']);
+                        unset($service[$nextBus]['VisitNumber']);
+                    } else {
+                        $service[$nextBus] = [];
+                    }
+                }
+                unset($service['NextBus3']);
+                $serviceNo = $service['ServiceNo'];
+                unset($service['ServiceNo']);
+                $services[$serviceNo] = $service;
+            }
+        }
+        ksort($services);
+    }
+    return $services;
+}
+?>
+
