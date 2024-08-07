a816f8c898b51839610e0272f4b253060aa20fb5
mattsoh <mattsoh100@gmail.com>
2024-08-07 22:36:54 +0800

    fixes

--- get_bookmarks.js
+++ get_bookmarks.js
@@ -1,11 +1,11 @@
 function getStops() {
     let stops = JSON.parse(localStorage.getItem('stops'));
     let stopNames = JSON.parse(localStorage.getItem('stopNames'));
-    console.log(stops,stopNames,(stops&&stopNames),stops.length == stopNames.length)
-    console.log(stops.length,stopNames.length)
-    console.log(stops,stopNames)
+    // console.log(stops,stopNames,(stops&&stopNames),stops.length == stopNames.length)
+    // console.log(stops.length,stopNames.length)
+    // console.log(stops,stopNames)
     if (stops && stopNames && stops.length == stopNames.length){
-        console.log("returning ", stops,stopNames)
+        // console.log("returning ", stops,stopNames)
         return [stops,stopNames];
     }else{ 
         return[[],[]];
@@ -20,17 +20,20 @@ function removeStop(stop){
         save(stops,stopNames);
     }
 }
-function queryBookmark(stop) {
+function query(stop) {
     var form = document.getElementById("busStopForm");
     form.elements['busStopCode'].value = stop;
     form.submit();
 }
 function getNearestStops() {
+    console.log("running");
     let options = {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
     };
+    let container = document.getElementById("nearby-stops");
+    let button = document.getElementById("nearby-button");
     function success(pos) {
         let latitude = pos.coords.latitude;
         let longitude = pos.coords.longitude;
@@ -38,32 +41,28 @@ function getNearestStops() {
         fetch(`/getNearestStops.php?lat=${latitude}&long=${longitude}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error('could not connect to server' + response.statusText);
+                window.alert(`Could not connect to server (${response.statusText}). This could be a fault on our part, but please make sure that you have an active internet connection.`);
             }
             return response.json();
         })
         .then(data => {
-            let container = document.getElementById("nearby-stops");
-            let button = document.getElementById("nearby-button");
-            console.log(data);
-
             container.innerHTML = "";
             data.forEach(stop => {
                 let stopElement = document.createElement("li");
-                stopElement.onclick = function(stop) { return function() { queryBookmark(stop[0]); } }(stop);
+                stopElement.onclick = function(stop) { return function() { query(stop); } }(stop);
                 stopElement.textContent = `${stop[1]} (${stop[0]})`;
+                stopElement.classList.add("stop");
                 container.appendChild(stopElement);
             });
             container.style.display = "";
             button.textContent = "refresh";
-        })  
-        // .catch(error => {
-        //     window.alert('Error finding nearest stops:', error);
-        // });
+            button.disabled = false;
+        }) 
     }
     function error(err) {
-        window.alert(`Unable to get current location (${err.code}: ${err.message})`);
+        window.alert(`We couldn't determine your current location (${err.message}). Please ensure that location services are enabled on your device.`);
     }
+    button.disabled = true;
     navigator.geolocation.getCurrentPosition(success,error,options);
 }
 
@@ -76,11 +75,12 @@ document.addEventListener('DOMContentLoaded', function() {
         let stopElement = document.createElement('li');
         let stopSpan = document.createElement('span');
 
-        stopElement.innerHTML = `${stopNames[i]} (<span class="bookmark-code" onclick="removeStop(${stops[i]})">${stops[i]}</span>)`;
-        stopElement.onclick = function(stop) { return function() { queryBookmark(stop); } }(stops[i]);
-        stopElement.classList.add("bookmark");
-        stopSpan.classList.add("bookmark-code")
-    
+        stopElement.textContent = `${stopNames[i]} `;
+        stopElement.onclick = function(stop) { return function() { query(stop); } }(stops[i]);
+        stopElement.classList.add("stop");
+        stopSpan.classList.add("code")
+        stopSpan.textContent = `(${stops[i]})`;
+        stopSpan.onclick = function(stop) { return function() { removeStop(stop); } }(stops[i]);
         stopElement.appendChild(stopSpan);
         container.appendChild(stopElement);
     }
