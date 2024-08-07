5315db67115831ccfb152e4ee731050837471ba9
mattsoh <mattsoh100@gmail.com>
2024-08-07 20:12:45 +0800

    implementing faster search method

--- getNearestStops.php
+++ getNearestStops.php
@@ -2,22 +2,31 @@
 header('Content-Type: application/json');
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (isset($_GET['lat']) && isset($_GET['long'])){
+        include 'gettimes.php';
         $lat = $_GET['lat'];
         $long = $_GET['long'];
-        include 'gettimes.php';
         $response = getAllData();
         if ($response == -1) return -1;
         $data = $_SERVER['stops'];
         $dists = [];
+        $closest = [[10],[10],[10],[10],[10]];
         foreach ($data as $stop) {
             $x = abs($stop['Latitude'] - $lat) * 111.12;
             $y = abs($stop['Longitude'] - $long) * 111.21;
-            array_push($dists, [sqrt($x * $x + $y * $y), $stop['BusStopCode'],$stop['Description']]) ;
+            $val = sqrt($x * $x + $y * $y);
+            for ($i = 0; $i < 5; $i++){
+                if ($val < $closest[$i]){
+                    array_splice($closest, $i, 0, [$val, $stop['BusStopCode'], $stop['Description']]);
+                    array_pop($closest);
+                    break;
+                }   
+            }
+            // array_push($dists, [sqrt($x * $x + $y * $y), $stop['BusStopCode'],$stop['Description']]) ;
         }
-        sort($dists);
+        // sort($dists);
         echo json_encode(array_slice(array_map(function($arr) {
             return array_slice($arr, 1);
-        }, $dists),0,10));
+        }, $closest),0,10));
     } else {
         http_response_code(400);
     }
