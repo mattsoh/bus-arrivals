ffcb8fabb50342d21cf536e33ab0ea0b9fac4bf9
mattsoh <mattsoh100@gmail.com>
2024-08-12 03:37:38 +0800

    more updates to zh site

--- zh/timings.php
+++ zh/timings.php
@@ -7,11 +7,11 @@
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stop = trim($_POST['busStopCode']);
             echo (is_numeric($stop) && strlen($stop) == 5 && getStop($stop) != NULL)
-                ? "Stop " . $stop . " Arrival Times"
-                : "Invalid Bus Stop Code";
+                ? $stop 。"站到达时间"
+                : "无效的巴士站代码";
         } else {
             http_response_code(405);
-            echo "Invalid request";
+            echo "无效的请求";
         }
         ?>
     </title>
@@ -24,14 +24,14 @@
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $stop = trim($_POST['busStopCode']);
             if (is_numeric($stop) && strlen($stop) == 5 && getStop($stop) != NULL) {
-                echo "Stop " . $stop;
+                echo  $stop . "站点";
             } else {
-                echo "Invalid bus stop code. Please enter a valid code.";
+                echo "无效的巴士站代码。请输入有效的代码。";
                 header("Location: /invalid.html");
                 exit();
             }
         } else {
-            echo "Invalid request method.";
+            echo "无效的请求方法。";
             exit();
         }
         ?></h1>
@@ -42,27 +42,27 @@
                 }
                 
             ?></h2>
-        <button id="bookmark" onclick="saveStop(<?php echo $stop?>)">Add bookmark</button>
+        <button id="bookmark" onclick="saveStop(<?php echo $stop?>)">添加收藏</button>
         <h2 id="notfound"><?php 
             $services = timings($stop);
-            if (empty($services)) echo "No bus services found.";
+            if (empty($services)) echo "未找到巴士服务。";
         ?></h2>
         <div id="slider-container" <?php echo (empty($services)) ? "style='display:none'" : "" ?>>
             
-            <label for="infoSlider">Extended Information: </label>
+            <label for="infoSlider">扩展信息：</label>
             <label id="switch">
                 <input type="checkbox" id="infoSlider" onchange="toggleExtendedInfo()">
                 <span id="slider"></span>
             </label>
         </div>
-        <a href="/">Back to home page</a>
+        <a href="/">返回主页</a>
         <div class="table-container" <?php echo (empty($services)) ? "style='display:none'" : "" ?>>
             <table>
                 <thead>
                     <tr>
                         <th></th>
-                        <th>Next</th>
-                        <th>Subsequent</th>
+                        <th>下一班</th>
+                        <th>随后的班次</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -78,12 +78,12 @@
                                 }else{
                                     $accessibility = $service[$nextBus]['Feature'] == "WAB" ? "inline" : "none";
                                     echo "<td>";
-                                    echo "<p class='arrival-time " . $service[$nextBus]['Load']."' mins='" . $service[$nextBus]['TimeRemaining'][0]."' secs='" . $service[$nextBus]['TimeRemaining'][1]."'>". (($service[$nextBus]['TimeRemaining'][0] > 0) ? $service[$nextBus]['TimeRemaining'][0]." mins" : "Arriving")." </p>";
+                                    echo "<p class='arrival-time " . $service[$nextBus]['Load']."' mins='" . $service[$nextBus]['TimeRemaining'][0]."' secs='" . $service[$nextBus]['TimeRemaining'][1]."'>". (($service[$nextBus]['TimeRemaining'][0] > 0) ? $service[$nextBus]['TimeRemaining'][0]." 分钟" : "到达")." </p>";
                                     echo "<div class='extended'>";
                                     echo "<br>";
-                                    echo "<span class='".$service[$nextBus]['Load']. "'>".(($service[$nextBus]['Load'] == "SEA") ? "Seats Available" : (($service[$nextBus]['Load'] == "SDA") ? "Standing Available" : "Limited Standing")) . "</span><br>";
-                                    echo (($service[$nextBus]['Type'] == "SD") ? "Single Deck" : (($service[$nextBus]['Type'] == "DD") ? "Double Deck" : "Bendy")) ."<br>";
-                                    if ($service[$nextBus]['Feature'] == "WAB") echo "♿ Accessable";
+                                    echo "<span class='".$service[$nextBus]['Load']. "'>".(($service[$nextBus]['Load'] == "SEA") ? "有座位" : (($service[$nextBus]['Load'] == "SDA") ? "有站立空间" : "有限站立空间")) . "</span><br>";
+                                    echo (($service[$nextBus]['Type'] == "SD") ? "单层" : (($service[$nextBus]['Type'] == "DD") ? "双层" : "弯曲")) ."<br>";
+                                    if ($service[$nextBus]['Feature'] == "WAB") echo "♿ 无障碍";
                                     echo "</div></td>";
                                 }
                                 
@@ -96,6 +96,6 @@
             </table>
         </div>
     </div>
-    <script src="update_arrivals.js"></script>
+    <script src="/zh/update_arrivals.js"></script>
 </body>
 </html>
---
+++ zh/update_search.js
@@ -0,0 +1,121 @@
+function getStops() {
+    let stops = JSON.parse(localStorage.getItem('stops'));
+    let stopNames = JSON.parse(localStorage.getItem('stopNames'));
+    // console.log(stops,stopNames,(stops&&stopNames),stops.length == stopNames.length)
+    // console.log(stops.length,stopNames.length)
+    // console.log(stops,stopNames)
+    if (stops && stopNames && stops.length == stopNames.length){
+        // console.log("returning ", stops,stopNames)
+        return [stops,stopNames];
+    }else{ 
+        return[[],[]];
+    }
+}
+function removeStop(stop){
+    var [stops,stopNames] = getStops();
+    if (stops.includes(stop)) {
+        let index = stops.indexOf(stop);
+        stops.splice(index, 1);
+        stopNames.splice(index,1);
+        if (stops.length == stops.length){
+            localStorage['stops'] = JSON.stringify(stops);
+            localStorage['stopNames'] = JSON.stringify(stopNames);
+        }else{
+            localStorage.clear();
+        }
+        console.log("saved");
+    }else{
+        console.log("stop not in bookmarks");
+    }
+    location.reload();
+}
+function query(stop) {
+    var form = document.getElementById("busStopForm");
+    form.elements['busStopCode'].value = stop;
+    form.submit();
+}
+function getNearestStops() {
+    console.log("running");
+    let options = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
+    };
+    let container = document.getElementById("nearby-stops");
+    let button = document.getElementById("nearby-button");
+    function success(pos) {
+        let latitude = pos.coords.latitude;
+        let longitude = pos.coords.longitude;
+        
+        fetch(`/getNearestStops.php?lat=${latitude}&long=${longitude}`)
+        .then(response => {
+            if (!response.ok) {
+                window.alert(`Could not connect to server (${response.statusText}). This could be a fault on our part, but please make sure that you have an active internet connection.`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            container.innerHTML = "";
+            data.forEach(stop => {
+                let stopElement = document.createElement("li");
+                stopElement.onclick = function(stop) { return function() { query(stop[0]); } }(stop);
+                stopElement.textContent = `${stop[1]} (${stop[0]})`;
+                stopElement.classList.add("stop");
+                container.appendChild(stopElement);
+            });
+            container.style.display = "";
+            button.textContent = "refresh";
+            button.disabled = false;
+        }) 
+    }
+    function error(err) {
+        window.alert(`We couldn't determine your current location (${err.message}). Please ensure that location services are enabled on your device.`);
+        button.disabled = false;
+    }
+    button.disabled = true;
+    navigator.geolocation.getCurrentPosition(success,error,options);
+}
+document.getElementById('busStopForm').addEventListener('submit', function(event) {
+    const input = document.getElementById('busStopCode');
+    const errorMessage = document.getElementById('error-message');
+    const submitButton = document.getElementById('submit-button');
+    
+    const value = input.value;
+    submitButton.classList.add('flash');
+    setTimeout(() => {
+        submitButton.classList.remove('flash');
+    }, 500);
+    if (/^\d{5}$/.test(value)) {
+        errorMessage.style.display = "none"; 
+        input.classList.remove('input-error');
+    } else {
+        event.preventDefault();
+        errorMessage.style.display = "block";
+        errorMessage.textContent = "Error! Please enter a 5-digit code.";
+        input.classList.add('input-error');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let [stops, stopNames] = getStops();
+    let container = document.getElementById('bookmarks');
+    // container.innerHTML = '';
+    console.log(stops);
+    if (stops.length == 0) container.style.display = "none";
+    for (var i = 0; i < stops.length; i++) {
+        let stopElement = document.createElement('li');
+        let nameSpan = document.createElement('span');
+        let stopSpan = document.createElement('span');
+
+        nameSpan.textContent = stopNames[i]+' ';
+        nameSpan.onclick = function(stop) { return function() { query(stop); } }(stops[i]);
+        stopElement.classList.add("stop");
+        stopSpan.classList.add("code")
+        stopSpan.textContent = `(${stops[i]})`;
+        stopSpan.onclick = function(stop) { return function() { removeStop(stop); } }(stops[i]);
+        stopElement.appendChild(nameSpan);
+        stopElement.appendChild(stopSpan);
+        console.log(nameSpan, stopSpan);
+        container.appendChild(stopElement);
+    } 
+});
